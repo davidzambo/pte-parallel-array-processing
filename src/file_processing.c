d73cf33a472e74611ccc86e1889baafde2acd080
David Zambo <dave@dcmf.io>
2021-11-14 20:08:57 +0100

    fix row validation based on input file length

--- src/file_processing.c
+++ src/file_processing.c
@@ -1,34 +1,40 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "structs/params.h"
 #include "file_processing.h"
 
+bool is_whitespace(char next_char_in_line) {
+    return next_char_in_line == ' ' ||
+        next_char_in_line == '\t' ||
+        next_char_in_line == '\n' ||
+        next_char_in_line == '\0';
+}
+
 void get_array_from_file(float **arr, struct Params params, FILE *file) {
-    int row = 0,
-            col = 0;
+    int row = 0, col = 0;
     char *line = NULL;
     size_t len = 0;
 
     char next_number_in_line[20];
     char next_char_in_line;
+    bool has_extra_row_in_file = false;
 
     while (getline(&line, &len, file) != -1) {
-        if (row > params.rows - 1) {
-            break;
-        }
         float number;
         int line_iterator = 0,
-                number_as_word_iterator = 0;
+            number_as_word_iterator = 0;
+
+
+        if (row > params.rows - 1) {
+            has_extra_row_in_file = true;
+            break;
+        }
 
         do {
             next_char_in_line = line[0 + line_iterator++];
-            if (
-                    next_char_in_line != ' ' &&
-                    next_char_in_line != '\t' &&
-                    next_char_in_line != '\n' &&
-                    next_char_in_line != '\0'
-                    ) {
+            if (!is_whitespace(next_char_in_line)) {
                 next_number_in_line[number_as_word_iterator++] = next_char_in_line;
                 continue;
             }
@@ -56,13 +62,13 @@ void get_array_from_file(float **arr, struct Params params, FILE *file) {
             exit(-1);
         }
 
-        row++;
+        ++row;
         col = 0;
     }
 
-    if (row != params.rows) {
+    if (row < params.rows || has_extra_row_in_file) {
         printf("Error during processing the given file: \n");
-        printf("The file contains %s rows than expected based on the given input parameters", (row > params.rows - 1 ) ? "more" : "less");
+        printf("The file contains %s rows than expected based on the given input parameters", (has_extra_row_in_file) ? "more" : "less");
         exit(-1);
     }
 
--- src/file_processing.h
+++ src/file_processing.h
@@ -1,6 +1,8 @@
 #ifndef PARALLEL_ARRAY_PROCESSING_FILE_PROCESSING_H
 #define PARALLEL_ARRAY_PROCESSING_FILE_PROCESSING_H
+#include <stdbool.h>
 
+bool is_whitespace(char next_char_in_line);
 void get_array_from_file(float **arr, struct Params params, FILE *file);
 
 #endif //PARALLEL_ARRAY_PROCESSING_FILE_PROCESSING_H
